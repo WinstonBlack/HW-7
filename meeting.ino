a2a92b81ead336888334ab472c5e16b0d2d36020
Winston Black <35714947+WinstonBlack@users.noreply.github.com>
2018-03-06 20:28:16 -0800

    Create meeting.ino

---
+++ meeting.ino
@@ -0,0 +1,37 @@
+void setup() {
+
+  pinMode(13, OUTPUT);
+}
+ 
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(250);       
+  // 
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);    
+  // 
+   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(250);       
+  // 
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);   
+  
+   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(250);       
+  // 
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);   
+
+   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(250);       
+  // 
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);   
+
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(250);       
+  // 
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);   
+}
