84f1c02c679b97774dade1a14b3f643a5ff5832d
Winston Black <35714947+WinstonBlack@users.noreply.github.com>
2018-03-06 20:19:06 -0800

    Update doctor.ino

--- doctor.ino
+++ doctor.ino
@@ -1,6 +1,6 @@
-// the setup function runs once when you press reset or power the board
+
 void setup() {
-  // initialize digital pin 13 as an output.
+
   pinMode(13, OUTPUT);
 }
  
@@ -9,9 +9,9 @@ void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);               // 
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(400);               // 
+  delay(1500);               // 
     digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // 
+  delay(4000);               // 
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(200); 
+  delay(500); 
 }
